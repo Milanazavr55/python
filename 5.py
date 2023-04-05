feb6b8c808f627beb361116f7e537c48e93fcabe
Milanazavr55 <105790948+Milanazavr55@users.noreply.github.com>
2023-04-05 17:55:50 +0700

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,21 @@
+s = {"имя": "Ыыы", "Фамилия": "Чо", "Отчество": "Ничо"}
+com = input("Каманда: ")
+if com == "Добавить запись":
+    newKey = input("Введите предмет: ")
+    s[newKey] = int(input("Введите аценку: "))
+for key in s:
+    print(key, ":", s[key])
+if com == "Изменить запись ":
+    
+    
+    
+    
+    
+    
+    
+#     
+#     uchenik[newKey] = int(input("Введите чото "))
+# if com == "Удалить":
+#     ubrat = input("Введите параметр: ")
+#     uchenik.pop(ubrat, None)
+# print(uchenik)
